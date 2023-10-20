9ddb3dd1b56d7f24c91c1af677c367a3afe194c7
vitaly <vitaliy69@mail.ru>
2023-10-20 14:52:28 +0300

    Добавляю в функции действия голос

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
+import time
 import random
 import speech_recognition
+from voice_output_gtts import speaker_assistant
 
 sr = speech_recognition.Recognizer()
 sr.pause_threshold = 0.5
 
 command_dict = {
     'commands': {
-        'greeting': ['Привет друг','Привет', 'Хай', 'Приветствую'],
-        'create_note': ['Добавить заметку', 'Заметка', 'Создать заметку'],
-        'play_music': ['Включи музыку', 'Музыку', 'Музыка']
+        'greeting': ['привет ника', 'привет', 'хай', 'приветствую'],
+        'create_note': ['добавить заметку', 'заметка', 'создать заметку'],
+        'play_music': ['включи музыку', 'музыку', 'музыка']
     }
 }
 
@@ -21,32 +23,35 @@ def listen_command():
         with speech_recognition.Microphone() as mic:
             sr.adjust_for_ambient_noise(source=mic, duration=0.5)
             audio = sr.listen(source=mic)
-            query = sr.recognize_google(audio_data=audio, language='ru-RU').capitalize()
+            query = sr.recognize_google(audio_data=audio, language='ru-RU').lower()
         return query
     except speech_recognition.UnknownValueError:
-        return 'Damm... Не понял что ты сказал :/'
+        return speaker_assistant('Я не поняла, пожалуйста повторите команду')
 
 
 def greeting():
     """Greeting function"""
-    return 'Привет дружище!'
+    return speaker_assistant('Привет! Я говорящий ассистент. Меня зовут Николь или просто Ника')
 
 
 def create_note():
     """Create a note"""
-    print('Какую добавим заметку?')
-
+    # print('Какую добавим заметку?')
+    speaker_assistant('Слушаю! Какую добавить заметку?')
+    # time.sleep(5)
     query = listen_command()
 
     with open('notes-list.txt', 'a') as file:
         file.write(f'{query}\n')
 
-    return f'Заметка: "{query}" добавлена в note-list'
+    return speaker_assistant(f'Я записала, Вы добавили заметку: {query}')
+
+    # return f'Заметка: "{query}" добавлена в note-list'
 
 
 def play_music():
     """Play a random mp3 file"""
-
+    speaker_assistant('Хорошо включаю')
     files = os.listdir('music')
     random_file = f'music/{random.choice(files)}'
     os.system(f'xdg-open {random_file}')
@@ -60,16 +65,6 @@ def main():
     for k, v in command_dict['commands'].items():
         if query in v:
             print(globals()[k]())
-
-
-    # if query == 'Привет друг':
-    #     print(greeting())
-    # elif query == 'Добавить заметку':
-    #     print(create_note())
-    # elif query == 'Включи музыку':
-    #     print(play_music())
-    # else:
-    #     print('Прожуй потом разговаривай! >:|')
 
 
 if __name__ == '__main__':
