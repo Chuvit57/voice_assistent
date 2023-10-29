5f0846a6d83af639ec258fa764baf5c12f0fca40
vitaly <vitaliy69@mail.ru>
2023-10-29 21:06:35 +0300

    Пока не решить проблему с постоянным прослушиванием(реагирует на шумы)

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import os
-import time
+import sys
+from selenium import webdriver
+from datetime import datetime
 import random
 import speech_recognition
 from voice_output_gtts import speaker_assistant
 
-sr = speech_recognition.Recognizer()
-sr.pause_threshold = 0.5
+chrome_driver_path = '~/Vitaly Education/Python_projects/voice_assistent/chromedriver'
+# Настройте переменную окружения PATH со ссылкой на путь к драйверу Chrome
+os.environ['PATH'] += ':' + chrome_driver_path
+
+
+# sr = speech_recognition.Recognizer()
+# sr.pause_threshold = 0.5
 
 command_dict = {
     'commands': {
         'greeting': ['привет ника', 'привет', 'хай', 'приветствую'],
         'create_note': ['добавить заметку', 'заметка', 'создать заметку'],
-        'play_music': ['включи музыку', 'музыку', 'музыка']
+        'play_music': ['включи музыку', 'музыку', 'музыка'],
+        'time_now': ['время', 'который час'],
+        'restart_pc': ['перезагрузка', 'перезагрузить компьютер'],
+        'cansel_restart_pc': ['отмена перезагрузки'],
+        'open_browser': ['браузер', 'открыть браузер'],
+        'exit': ['выход', 'завершение работы']
     }
 }
 
 
 def listen_command():
     """The function will return the recognized command"""
+    sr = speech_recognition.Recognizer()
+    sr.pause_threshold = 0.5
 
     try:
         with speech_recognition.Microphone() as mic:
@@ -45,8 +59,12 @@ def create_note():
         file.write(f'{query}\n')
 
     return speaker_assistant(f'Я записала, Вы добавили заметку: {query}')
-
     # return f'Заметка: "{query}" добавлена в note-list'
+
+
+def time_now():
+    time_checker = datetime.now()
+    speaker_assistant(f"Сейчас питерское время: {time_checker.hour} точка {time_checker.minute} ")
 
 
 def play_music():
@@ -58,12 +76,74 @@ def play_music():
     return f'Танцуй под {random_file.split("/")[-1]}'
 
 
-def main():
-    # print(globals()) - это глобальная функция
+def restart_pc():
+    os.system('shutdown -r +5')
+    speaker_assistant('Компьютер будет перезагружен через 5 минут. Для отмены скажите: отмена')
+
+
+def cansel_restart_pc():
+    os.system('shutdown -c')
+    speaker_assistant('Перезагрузка компьютере успешно отменена')
+
+
+def open_browser():
+    speaker_assistant('Какую страницу вы хотите открыть?')
+
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)
+    driver = webdriver.Chrome()
+
+    driver.implicitly_wait(1)
+    driver.maximize_window()
+
     query = listen_command()
 
+    if 'google' in query:
+        speaker_assistant('Что будем искать?')
+
+        query = listen_command()
+        query = query.split()
+
+        driver.get('https://www.google.com/search?q=' + '+'.join(query))
+        speaker_assistant(f'Поиск по {query} успешно запущен')
+
+    elif 'youtube' in query:
+        speaker_assistant('Что будем смотреть?')
+
+        query = listen_command()
+        query = query.split()
+
+        driver.get('https://www.youtube.com/results?search_query=' + '+'.join(query))
+        speaker_assistant('Приятного просмотра')
+
+        return
+
+    else:
+        speaker_assistant('Не понял тебя')
+
+        return
+
+
+def exit():
+    speaker_assistant('Спасибо! До свидание')
+    sys.exit()
+
+
+# while True:
+#     listen_command()
+#     for k, v in command_dict['commands'].items():
+#         if query in v:
+#             execute = getattr()
+#
+#     except Exception as _ex:
+#     print('Команда не распознана')
+#     sys,exit()
+
+
+def main():
+    listen_command()
     for k, v in command_dict['commands'].items():
-        if query in v:
+        if listen_command() in v:
             print(globals()[k]())
 
 
--- voice_output_gtts.py
+++ voice_output_gtts.py
@@ -1,11 +1,28 @@
 from gtts import gTTS
 import pygame
-import os
 import tempfile
+import time
+from datetime import datetime, date, time
+
+time_checker = datetime.now()
+
 
 # Создание экземпляра класса gTTS
-text = "Привет! Я говорящий ассистент. Меня зовут Николь или просто ника"
-tts = gTTS(text, lang='ru')
+
+def speaker_assistant(text):
+    tts = gTTS(text, lang='ru')
+    temp_file = tempfile.NamedTemporaryFile(suffix=".mp3", delete=False)
+    tts.save(temp_file.name)
+    pygame.init()
+    pygame.mixer.music.load(temp_file.name)
+    pygame.mixer.music.load(temp_file.name)
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+    pygame.mixer.music.stop()
+    pygame.mixer.quit()
 
 # Это пример с сохранением записи в файл----------------------------------------------
 # Сохранение аудио во временный файл
@@ -15,23 +32,27 @@ tts = gTTS(text, lang='ru')
 # pygame.mixer.music.load('tts_google1.mp3')
 
 
-# Сохранение аудио во временный файл -----------------------------------
-temp_file = tempfile.NamedTemporaryFile(suffix=".mp3", delete=False)
-tts.save(temp_file.name)
+# # Сохранение аудио во временный файл -----------------------------------
+# temp_file = tempfile.NamedTemporaryFile(suffix=".mp3", delete=False)
+# tts.save(temp_file.name)
 
 # Инициализация библиотеки pygame
-pygame.init()
+# pygame.init()
 
 # Загрузка аудиофайла в Pygame
-pygame.mixer.music.load(temp_file.name)
+# pygame.mixer.music.load(temp_file.name)
 
 # Воспроизведение аудио
-pygame.mixer.music.play()
+# pygame.mixer.music.play()
 
 # Ожидание окончания проигрывания
-while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
+# while pygame.mixer.music.get_busy():
+#     pygame.time.Clock().tick(10)
 
 # Удаление временного файла
-pygame.mixer.music.stop()
-pygame.mixer.quit()
+# pygame.mixer.music.stop()
+# pygame.mixer.quit()
+
+
+# speaker_assistant(f"Привет! Я говорящий ассистент. Меня зовут Николь или просто Ника. Сейчас: {time_checker.hour} часа "
+#         f"{time_checker.minute} минут")
